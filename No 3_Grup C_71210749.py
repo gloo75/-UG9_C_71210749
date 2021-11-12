bea6b0cec637f9127083d22ed8a4ea91082a1f08
gloo75 <93343381+gloo75@users.noreply.github.com>
2021-11-12 10:54:17 +0700

    Add files via upload

---
+++ No 3_Grup C_71210749.py
@@ -0,0 +1,21 @@
+#SOAL NO 3 a
+
+a = int(input("Masukan alas atap: "))
+ta = int(input("Masukan tinggi atap: "))
+tt = int(input("Masukan tinggi tembok: "))
+
+luas = (a * tt * 0.5) + (tt * tt)
+semen = luas / 5
+
+print("rumah membutuhkan", semen, "sak semen")
+
+
+#SOAL NO 3 b
+a = int(input("Masukan alas atap: "))
+ta = int(input("Masukan tinggi atap: "))
+tt = int(input("Masukan tinggi tembok: "))
+
+luas = (a * tt * 0.5) + (tt * tt)
+semen = luas / 5
+
+print("rumah membutuhkan", semen, "sak semen")
