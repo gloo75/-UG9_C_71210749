fccbc40d49be2c9591d0effaee1229bebcb0be73
gloo75 <93343381+gloo75@users.noreply.github.com>
2021-11-12 10:52:45 +0700

    Add files via upload

---
+++ No 1_Grup C_71210749.py
@@ -0,0 +1,17 @@
+#SOAL N0 1 a
+#rumus phi x r^2
+
+phi = 3.14
+r = int(input("Masukan jari-jari : "))
+luas = phi * ( r * r )
+
+print (luas)
+
+#SOAL 1 b
+#rumus : phi x r^2
+
+phi = 3.14
+r = int(input("Masukan jari-jari : "))
+luas = phi * ( r * r )
+
+print (luas)
