7f429968e4c06d475a4a7281072cb837c65f1ec3
gloo75 <93343381+gloo75@users.noreply.github.com>
2021-11-12 10:54:54 +0700

    Add files via upload

---
+++ Np 4_Grup C_71210749.py
@@ -0,0 +1,10 @@
+#SOAL NO 4
+
+a = int(input("Masukkan suku awal: ")
+b = int(input("Masukkan beda: ")
+n = int(input("Masukkan suku ke-99: ")
+
+
+Sn = n/2 * (2 * a+(n-1) * b)
+
+print("Jumlah suku ke-99: ", Sn)
